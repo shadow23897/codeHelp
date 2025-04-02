75be7d23d8d22341d13863e4c4f4a0071941238c
shadow23897 <support@pc-custom.be>
2025-04-02 23:00:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import os
 import subprocess
 import sys
+import ctypes
 
 def run_as_admin():
-
     if os.name == 'nt':
         if not ctypes.windll.shell32.IsUserAnAdmin():
             print("\033[91mDemande d'exécution en mode administrateur...\033[0m")
             try:
                 ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
                 sys.exit()
-            except:
+            except Exception as e:
                 print("\033[91mÉchec de la demande d'administrateur.\033[0m")
+                print(e)
 
 def banner():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -20,46 +21,50 @@ def banner():
     print("░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ ")
     print("░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ ")
     print("░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓██████▓▒░ ░▒▓████████▓▒░▒▓██████▓▒░ ░▒▓█▓▒░      ░▒▓███████▓▒░  ")
-    print("░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░        ")
-    print("░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░        ")
+    print("░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ ")
+    print("░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ ")
     print(" ░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓███████▓▒░░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓████████▓▒░▒▓█▓▒░        ")
-    print("\033[91m══════════════════════════════════════════════════════════\033[0m")
-    print("\033[91m  https://github.com/shadow23897/codeHelp/\033[0m")
-    print("\033[91m══════════════════════════════════════════════════════════\033[0m")
-
+    print("\033[91m════════════════════════════════════════════════════════════════════════════\033[0m")
+    print("\033[91m  https://github.com/shadow23897/codeHelp/\n  https://discord.gg/codehelp\033[0m")
+    print("\033[91m════════════════════════════════════════════════════════════════════════════\033[0m")
+    print("\033[0m")
 
 def main_menu():
-    banner()
-    print("\033[92m[1] - System info\033[0m")
-    print("\033[92m[2] - System cleaner\033[0m")
-    print("\033[92m[3] - website clone\033[0m")
-    print("\033[92m[4] - bloqueur de pub free\033[0m")
-    print("\033[92m[5] - verify link virus\033[0m")
-    print("\033[92m[6] - Quitter\033[0m")
-    choix = input("\nChoisissez une option : ")
+    while True:
+        banner()
+        print("\033[92m[1] - System info\033[0m")
+        print("\033[92m[2] - System cleaner\033[0m")
+        print("\033[92m[3] - Website clone\033[0m")
+        print("\033[92m[4] - Bloqueur de pub free\033[0m")
+        print("\033[92m[5] - Verify link virus\033[0m")
+        print("\033[92m[6] - Quitter\033[0m")
+        choix = input("\nChoisissez une option : ")
 
-    if choix == "1":
-        run_script("1.py")
-    elif choix == "2":
-        run_script("2.py")
-    elif choix == "3":
-        run_script("3.py")
-    elif choix == "4":
-        run_script("4.py")
-    elif choix == "5":
-        run_script("5.py")
-    elif choix == "6":
-        print("\n\033[91mFermeture du programme...\033[0m")
-        exit()
-    else:
-        print("\033[91mOption invalide, veuillez réessayer.\033[0m")
-        main_menu()
+        if choix == "1":
+            run_script("1.py")
+        elif choix == "2":
+            run_script("2.py")
+        elif choix == "3":
+            run_script("3.py")
+        elif choix == "4":
+            run_script("4.py")
+        elif choix == "5":
+            run_script("5.py")
+        elif choix == "6":
+            print("\n\033[91mFermeture du programme...\033[0m")
+            break
+        else:
+            print("\033[91mOption invalide, veuillez réessayer.\033[0m")
 
 def run_script(script_name):
     script_path = os.path.join("src", script_name)
     if os.path.exists(script_path):
         if os.name == 'nt':
-            subprocess.run(["powershell", "Start-Process", "python", f"-ArgumentList {script_path}", "-Verb", "RunAs"], check=True)
+            subprocess.run([
+                "powershell",
+                "-Command",
+                f"Start-Process cmd -ArgumentList '/k python \"{script_path}\"' -Verb RunAs"
+            ], check=True)
         else:
             subprocess.run(["python", script_path], check=True)
     else:
@@ -67,6 +72,6 @@ def run_script(script_name):
 
 if __name__ == "__main__":
     if os.name == 'nt':
-        import ctypes
         run_as_admin()
     main_menu()
+
