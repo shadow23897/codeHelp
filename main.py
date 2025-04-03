d878bb379541f1994f4e4de2ca589da8756b9940
shadow23897 <support@pc-custom.be>
2025-04-03 16:46:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ def run_as_admin():
             print("\033[91mDemande d'exécution en mode administrateur...\033[0m")
             try:
                 ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, __file__, None, 1)
-                sys.exit()
             except Exception as e:
                 print("\033[91mÉchec de la demande d'administrateur.\033[0m")
                 print(e)
+            # Ferme immédiatement le processus d'origine pour que la fenêtre se ferme
+            os._exit(0)
 
 def banner():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -21,13 +22,12 @@ def banner():
     print("░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ ")
     print("░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ ")
     print("░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓██████▓▒░ ░▒▓████████▓▒░▒▓██████▓▒░ ░▒▓█▓▒░      ░▒▓███████▓▒░  ")
-    print("░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ ")
-    print("░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ ")
+    print("░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░ ")
+    print("░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░        ")
     print(" ░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓███████▓▒░░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓████████▓▒░▒▓█▓▒░        ")
-    print("\033[91m════════════════════════════════════════════════════════════════════════════\033[0m")
-    print("\033[91m  https://github.com/shadow23897/codeHelp/\n  https://discord.gg/codehelp\033[0m")
-    print("\033[91m════════════════════════════════════════════════════════════════════════════\033[0m")
-    print("\033[0m")
+    print("\033[91m══════════════════════════════════════════════════════════\033[0m")
+    print("\033[91m  https://github.com/shadow23897/codeHelp/ \n  https://discord.gg/codehelp\033[0m")
+    print("\033[91m══════════════════════════════════════════════════════════\033[0m")
 
 def main_menu():
     while True:
@@ -37,7 +37,8 @@ def main_menu():
         print("\033[92m[3] - Website clone\033[0m")
         print("\033[92m[4] - Bloqueur de pub free\033[0m")
         print("\033[92m[5] - Verify link virus\033[0m")
-        print("\033[92m[6] - Quitter\033[0m")
+        print("\033[92m[6] - ip lookup\033[0m")
+        print("\033[92m[7] - Quitter\033[0m")
         choix = input("\nChoisissez une option : ")
 
         if choix == "1":
@@ -51,6 +52,8 @@ def main_menu():
         elif choix == "5":
             run_script("5.py")
         elif choix == "6":
+            run_script("6.py")
+        elif choix == "7":
             print("\n\033[91mFermeture du programme...\033[0m")
             break
         else:
@@ -74,4 +77,5 @@ if __name__ == "__main__":
     if os.name == 'nt':
         run_as_admin()
     main_menu()
+
 
